a604914a213fc4a19b52356cf7f777990a59dd58
Kenzo <totallykenzo@gmail.com>
2024-08-16 08:49:07 +1000

    Add Github and Youtube, removed unfinished code

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
 function closestrobe() {
     document.getElementById("strobeWarning").style.display = "none";
 }
+
+function disablestrobe() {
+    document.getElementsByName().style.background = "black";
+}
