d12a12ec0bd5f0ee3e9b991bd39fe828a7b4dd0f
Kenzo <totallykenzo@gmail.com>
2024-08-16 12:17:03 +1000

    started cookie

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 function closestrobe() {
+    // @ts-ignore
+    document.getElementById("strobeWarning").style.display = "none";
+    document.cookie = "strobeagree=1";
+}
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+function checkCookie() {
+    var strobeagree = getCookie("strobeagree");
+    if (strobeagree == "1") {
+        return true;
+    }
+}
+if (checkCookie()) {
+    // @ts-ignore
     document.getElementById("strobeWarning").style.display = "none";
 }
--- index.ts
+++ index.ts
@@ -1,7 +1,33 @@
 function closestrobe() {
+    // @ts-ignore
     document.getElementById("strobeWarning").style.display = "none";
+    document.cookie = "strobeagree=1";
 }
 
-function disablestrobe() {
-    document.getElementsByName().style.background = "black";
+function getCookie(cname: string) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    let strobeagree = getCookie("strobeagree");
+    if (strobeagree == "1") {
+        return true;
+    }
+}
+
+if (checkCookie()) {
+    // @ts-ignore
+    document.getElementById("strobeWarning").style.display = "none";
 }
