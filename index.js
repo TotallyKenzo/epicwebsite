f0381667a36ed62e44653726b43683c079413ecd
Kenzo <totallykenzo@gmail.com>
2024-08-16 12:24:25 +1000

    changed to local storage

--- index.js
+++ index.js
@@ -1,30 +1,11 @@
 function closestrobe() {
     // @ts-ignore
     document.getElementById("strobeWarning").style.display = "none";
-    document.cookie = "strobeagree=1";
+    localStorage.setItem("strobe", "True");
 }
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
+function strobe() {
+    if (localStorage.getItem("strobe") === "True") {
+        // @ts-ignore
+        document.getElementById("strobeWarning").style.display = "none";
     }
-    return "";
-}
-function checkCookie() {
-    var strobeagree = getCookie("strobeagree");
-    if (strobeagree == "1") {
-        return true;
-    }
-}
-if (checkCookie()) {
-    // @ts-ignore
-    document.getElementById("strobeWarning").style.display = "none";
 }
--- index.ts
+++ index.ts
@@ -1,33 +1,12 @@
 function closestrobe() {
     // @ts-ignore
     document.getElementById("strobeWarning").style.display = "none";
-    document.cookie = "strobeagree=1";
+    localStorage.setItem("strobe", "True");
 }
 
-function getCookie(cname: string) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i <ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
+function strobe() {
+    if (localStorage.getItem("strobe") === "True") {
+        // @ts-ignore
+        document.getElementById("strobeWarning").style.display = "none";
     }
-    return "";
-}
-
-function checkCookie() {
-    let strobeagree = getCookie("strobeagree");
-    if (strobeagree == "1") {
-        return true;
-    }
-}
-
-if (checkCookie()) {
-    // @ts-ignore
-    document.getElementById("strobeWarning").style.display = "none";
 }
